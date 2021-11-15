82fe01fa22eaf3d891514a24644cd994f1b726ee
Stephane Bisson <sbisson@wikimedia.org>
2021-11-15 10:24:59 -0500

    Show only 25 items at a time

--- viewer/src/components/Feed.vue
+++ viewer/src/components/Feed.vue
@@ -10,7 +10,7 @@
         name: 'Feed',
         components: { FeedLine },
         computed: {
-            feedLines: function() { return this.shuffle(data); }
+            feedLines: function() { return this.shuffle(data).slice(25); }
         },
         methods: {
             shuffle(arr) {
